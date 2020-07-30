bb778b49a0304d68e75939d62c3294139686ad99
jeffzyliu <jeffzyliu@gmail.com>
2020-07-30 19:13:57 -0400

    added dynamic tagging

--- server.js
+++ server.js
@@ -4,28 +4,7 @@ import morgan from 'morgan';
 // Imports the Google Cloud client libraries
 import vision from '@google-cloud/vision';
 
-// Creates a client
-const client = new vision.ImageAnnotatorClient();
-
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// const bucketName = 'Bucket where the file resides, e.g. my-bucket';
-// const fileName = 'Path to file within bucket, e.g. path/to/image.png';
-
-// Performs label detection on the gcs file
-
-async function testVision() {
-  const [result] = await client.labelDetection(
-    // `gs://${bucketName}/${fileName}`
-    'gs://cloud-samples-data/vision/label/setagaya.jpeg',
-  );
-  const labels = result.labelAnnotations;
-  console.log('Labels:');
-  labels.forEach((label) => { console.log(label.description); });
-  return labels;
-}
-
+// express setup
 const app = express();
 
 app.use(morgan('dev'));
@@ -33,9 +12,41 @@ app.use(morgan('dev'));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+// const bucketName = 'Bucket where the file resides, e.g. my-bucket';
+// const fileName = 'Path to file within bucket, e.g. path/to/image.png';
+// Performs label detection on the gcs file
+async function testVision(uri) {
+  const [result] = await client.labelDetection(uri);
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach((label) => { console.log(label.description); });
+  return labels;
+}
+
+// routes
+
+// test a hardcoded photo
 app.get('/', async (req, res) => {
   try {
-    const results = await testVision();
+    const results = await testVision(
+      'gs://cs52-firebase-ml-demo.appspot.com/photos/second album/dogs.jpg',
+    );
+    const cleanedResults = results.map((label) => { return label.description; });
+    res.json(cleanedResults);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// get a dynamic google cloud bucket URI
+app.post('/api', async (req, res) => {
+  try {
+    const { uri } = req.body;
+    const results = await testVision(uri);
     const cleanedResults = results.map((label) => { return label.description; });
     res.json(cleanedResults);
   } catch (error) {
